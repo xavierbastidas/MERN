b1c4f8c2285aba3022e9558318b46425f69bee5d
jonabastidas <bastidasjonathan140148@gmail.com>
2023-03-07 19:02:13 -0500

    apinodejs1

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { login, register ,infoUser ,refreshToken, logout} from "../controllers/auth.controller.js";
 import { requireToken } from "../middlewares/requireToken.js";
-import { requireRefreshToken } from "../middlewares/requireRefreshToken";
+import { requireRefreshToken } from "../middlewares/requireRefreshToken.js";
 import { bodyLoginValidator, bodyRegisterValidator } from "../middlewares/validatorManager.js";
 const router = Router();
 router.post('/register',bodyRegisterValidator,register);
